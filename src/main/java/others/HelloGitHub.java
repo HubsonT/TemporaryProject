9c81b560f0bce61bc06d46cee079aed2f6ced59a
HubsonT <hubsonpanter@gmail.com>
2017-09-30 14:57:21 +0200

    junk line 2

--- src/main/java/others/HelloGitHub.java
+++ src/main/java/others/HelloGitHub.java
@@ -19,6 +19,7 @@ public class HelloGitHub {
 	System.out.println("Paragraph1");
 	System.out.println("Paragraph2 w ramach Feature1");
         System.out.println("Junk line 1");
+        System.out.println("Junk line 2");
     }
     
 }
