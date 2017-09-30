cc378d7209a9a04263a35f4e49860fdf4af8756d
HubsonT <hubsonpanter@gmail.com>
2017-09-30 18:00:49 +0200

    paragraf 3 dodany f2

--- src/main/java/others/HelloGitHub.java
+++ src/main/java/others/HelloGitHub.java
@@ -21,7 +21,7 @@ public class HelloGitHub {
         System.out.println("Junk line 1");
         System.out.println("Junk line 2");
         System.out.println("Junk line 3");
-        System.out.println("Paragraph 3 w ramach Feature2");
+        System.out.println("Paragraph 3 dodany w ramach Feature2");
     }
     
 }
