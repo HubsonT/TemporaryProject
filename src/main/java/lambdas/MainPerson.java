e970bb441382496df1acecf284ea4e52385d5583
HubsonT <hubsonpanter@gmail.com>
2017-10-07 14:14:43 +0200

    dodanie interfejsu funkcyjnego Function aby wyszukiwać adresy email osób spełniających założone kryteria

--- src/main/java/lambdas/MainPerson.java
+++ src/main/java/lambdas/MainPerson.java
@@ -5,6 +5,7 @@
  */
 package lambdas;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +26,10 @@ public class MainPerson {
         Person p5 = new Person("Magda Nowak", Person.Sex.FEMALE, 22);
         Person p6 = new Person("Krystyna Nowakowska", Person.Sex.FEMALE, 50);
         Person p7 = new Person("Zygmunt Frojd", Person.Sex.MALE, 26);
+        Person p8 = new Person("Adam Krakus", LocalDate.of(1999, 10, 22), Person.Sex.MALE, "adam@adam.com", 18);
+        Person p9 = new Person("Rysiek Krakus", LocalDate.of(1995, 11, 11), Person.Sex.MALE, "rysiek@rysiek.com", 22);
+        Person p10 = new Person("Maciek Krakus", LocalDate.of(1993, 10, 22), Person.Sex.MALE, "maciek@maciek.com", 24);
+        
 
         listaOsob.add(p1);
         listaOsob.add(p2);
@@ -33,14 +38,18 @@ public class MainPerson {
         listaOsob.add(p5);
         listaOsob.add(p6);
         listaOsob.add(p7);
+        listaOsob.add(p8);
+        listaOsob.add(p9);
+        listaOsob.add(p10);
 
         //p1.printPerson();
-        for (int i = 0; i < listaOsob.size(); i++) {
-            System.out.println(listaOsob.get(i));
-        }
+        
+//        for (int i = 0; i < listaOsob.size(); i++) {
+//            System.out.println(listaOsob.get(i));
+//        }
 
-        Person.printPersonOlderThan(listaOsob, 22);
-        Person.printPersonsWithinAgeRange(listaOsob, 18, 25);
+        //Person.printPersonOlderThan(listaOsob, 22);
+        //Person.printPersonsWithinAgeRange(listaOsob, 18, 25);
 
         //Person.printPersons(listaOsob, new CheckPersonEligibleForSelectiveService());
         //użycie klasy anonimowej
@@ -66,6 +75,20 @@ public class MainPerson {
                 && p.getAge() <= 25,
                 p -> p.printPerson()
         );
+        
+        Person.processPersonsWithFunction(listaOsob,
+                p -> p.getGender()==Person.Sex.MALE
+                && p.getAge()>=18
+                && p.getAge()<=25,
+                p-> p.getEmailAddress(),
+                email -> System.out.println(email)
+        
+        );
+                
+                
+                
+                
+        
 
     }
 
--- src/main/java/lambdas/Person.java
+++ src/main/java/lambdas/Person.java
@@ -8,6 +8,7 @@ package lambdas;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -18,18 +19,17 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  */
 public class Person {
 
-
     public enum Sex {
         MALE, FEMALE;
     }
-    
+
     String name;
     LocalDate birthday;
     Sex gender;
     String emailAddress;
     int age;
-    
-    public int getAge(){
+
+    public int getAge() {
         return age;
     }
 
@@ -52,7 +52,7 @@ public class Person {
     @Override
     public String toString() {
         //return "Person{" + "name=" + name + ", birthday=" + birthday + ", gender=" + gender + ", emailAddress=" + emailAddress + ", age=" + age + '}';
-         ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+        ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE);
         return builder.append(name).append(birthday).append(gender).append(emailAddress).append(age).toString();
     }
 
@@ -63,37 +63,33 @@ public class Person {
         this.emailAddress = emailAddress;
         this.age = age;
     }
-    
-    
+
     public Person(String name, Sex gender, int age) {
-        this(name,LocalDate.of(1970,01,01),gender,"none@none.pl",age);
+        this(name, LocalDate.of(1970, 01, 01), gender, "none@none.pl", age);
     }
-    
-    
-    
+
     public void printPerson() {
         System.out.println(this.toString());
     }
-    
-    
+
     public static void printPersonOlderThan(List<Person> roster, int age) {
         System.out.println("lista osób starszych niż " + age);
-        for(Person p: roster) {
-            if (p.getAge()> age) {
+        for (Person p : roster) {
+            if (p.getAge() > age) {
                 p.printPerson();
             }
         }
     }
-    
+
     public static void printPersonsWithinAgeRange(List<Person> roster, int low, int high) {
-        System.out.println("Lista osób starszych niż " + low + " i młodszych niż " + high );
-        for (Person p: roster) {
-            if (p.getAge()>low && p.getAge()<high) {
+        System.out.println("Lista osób starszych niż " + low + " i młodszych niż " + high);
+        for (Person p : roster) {
+            if (p.getAge() > low && p.getAge() < high) {
                 p.printPerson();
             }
         }
     }
-    
+
 //    public static void printPersons(List<Person> rooster, CheckPerson tester) {
 //        System.out.println("Lista osób spełniających określone kryteria");
 //        for(Person p: rooster){
@@ -102,30 +98,38 @@ public class Person {
 //            }
 //        }
 //    }
-    
     //zastąpienie powyższej metody opertej na interfejsie nową implementacją zastępującą interfejs 
     //zdefiniowany przez nas interfejsem funkcjonalnym Predicate zdefiniowanym w java.util.function
-    
-    
     public static void printPersonsWithPredicate(List<Person> rooster, Predicate<Person> tester) {
         System.out.println("Lista osób spełniających określone kryteria");
-        for(Person p: rooster) {
-            if (tester.test(p)){
+        for (Person p : rooster) {
+            if (tester.test(p)) {
                 p.printPerson();
             }
         }
     }
+
     //zastosowanie dodatkowego interfejsu funkconalnego Consumer<T> aby zastąpić wywołanie metody printPerson w ciele metody, 
     //wywołaniem metody printPerson przy wywołaniu metody
     public static void processPersons(List<Person> rooster, Predicate<Person> tester, Consumer<Person> block) {
-        for (Person p:rooster) {
+        for (Person p : rooster) {
             if (tester.test(p)) {
                 block.accept(p);
             }
         }
     }
-    
-    
-    
-    
+
+    public static void processPersonsWithFunction(
+            List<Person> rooster,
+            Predicate<Person> tester,
+            Function<Person, String> mapper,
+            Consumer<String> block) {
+        for (Person p : rooster) {
+            if (tester.test(p)) {
+                String data = mapper.apply(p);
+                block.accept(data);
+            }
+        }
+    }
+
 }
