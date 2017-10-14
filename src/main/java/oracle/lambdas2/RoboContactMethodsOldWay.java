6f253bdc43f24d21dc8ef01bc5ed99c53428b1ae
HubsonT <hubsonpanter@gmail.com>
2017-10-14 12:15:45 +0200

    drobne zmiany w RoboContactMethodsOldWay

--- src/main/java/oracle/lambdas2/RoboContactMethodsOldWay.java
+++ src/main/java/oracle/lambdas2/RoboContactMethodsOldWay.java
@@ -14,16 +14,17 @@ import java.util.List;
 public class RoboContactMethodsOldWay {
     public void callDrivers(List<Person> pl) {
        for (Person p: pl) {
-           if (p.getAge()>=16) {
+           if (isDriver(p)) {
                roboCall(p);
            }
        }
     }
+
     
     
     public void emailDraftees(List<Person> pl) {
         for (Person p: pl) {
-            if (p.getAge()>=18 && p.getAge()<=25 && p.getGender()==Gender.MALE) {
+            if (isDraftee(p)) {
                 roboEmail(p);
             }
         }
@@ -32,11 +33,24 @@ public class RoboContactMethodsOldWay {
     
     public void mailPilots(List<Person> pl) {
         for (Person p: pl) {
-            if (p.getAge()>=23 && p.getAge()<=65) {
+            if (isPilot(p)) {
                 roboMail(p);
             }
         }
     }
+
+    public static boolean isPilot(Person p) {
+        return p.getAge()>=23 && p.getAge()<=65;
+    }
+
+    public static boolean isDraftee(Person p) {
+        return p.getAge()>=18 && p.getAge()<=25 && p.getGender()==Gender.MALE;
+    }
+    
+    public static boolean isDriver(Person p) {
+        return p.getAge()>=16;
+    }
+    
 
     private void roboCall(Person p) {
         System.out.println("Calling " + p.getGivenName() + " " + p.getSurName() + " age " + p.getAge() + " at " + p.getPhone());
