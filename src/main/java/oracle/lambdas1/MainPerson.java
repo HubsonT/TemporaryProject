2523db9be08d13820b1a00e93f6c2b8a393ff882
HubsonT <hubsonpanter@gmail.com>
2017-10-21 13:19:34 +0200

    drobne modyfikacje pliku MainPerson

--- src/main/java/oracle/lambdas1/MainPerson.java
+++ src/main/java/oracle/lambdas1/MainPerson.java
@@ -8,17 +8,18 @@ package oracle.lambdas1;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author Hubert
  */
 public class MainPerson {
-
+    
     public static void main(String[] args) {
-
-        List<Person> listaOsob = new ArrayList<>();
-
+        
+        List<Person> people = new ArrayList<>();
+        
         Person p1 = new Person("Janek Kowalski", Person.Sex.MALE, 15);
         Person p2 = new Person("Wanda Nowak", Person.Sex.FEMALE, 24);
         Person p3 = new Person("Marek Ogarek", Person.Sex.MALE, 19);
@@ -30,27 +31,23 @@ public class MainPerson {
         Person p9 = new Person("Rysiek Krakus", LocalDate.of(1995, 11, 11), Person.Sex.MALE, "rysiek@rysiek.com", 22);
         Person p10 = new Person("Maciek Krakus", LocalDate.of(1993, 10, 22), Person.Sex.MALE, "maciek@maciek.com", 24);
         
-
-        listaOsob.add(p1);
-        listaOsob.add(p2);
-        listaOsob.add(p3);
-        listaOsob.add(p4);
-        listaOsob.add(p5);
-        listaOsob.add(p6);
-        listaOsob.add(p7);
-        listaOsob.add(p8);
-        listaOsob.add(p9);
-        listaOsob.add(p10);
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        people.add(p4);
+        people.add(p5);
+        people.add(p6);
+        people.add(p7);
+        people.add(p8);
+        people.add(p9);
+        people.add(p10);
 
         //p1.printPerson();
-        
 //        for (int i = 0; i < listaOsob.size(); i++) {
 //            System.out.println(listaOsob.get(i));
 //        }
-
         //Person.printPersonOlderThan(listaOsob, 22);
         //Person.printPersonsWithinAgeRange(listaOsob, 18, 25);
-
         //Person.printPersons(listaOsob, new CheckPersonEligibleForSelectiveService());
         //użycie klasy anonimowej
 //        Person.printPersons(listaOsob, new CheckPerson() {
@@ -69,27 +66,31 @@ public class MainPerson {
 //                && p.getAge() >= 18
 //                && p.getAge() <= 25
 //        );
-        Person.processPersons(listaOsob,
+        Person.processPersons(people,
                 p -> p.getGender() == Person.Sex.MALE
                 && p.getAge() >= 18
                 && p.getAge() <= 25,
                 p -> p.printPerson()
         );
         
-        Person.processPersonsWithFunction(listaOsob,
-                p -> p.getGender()==Person.Sex.MALE
-                && p.getAge()>=18
-                && p.getAge()<=25,
-                p-> p.getEmailAddress(),
+        Person.processPersonsWithFunction(people,
+                p -> p.getGender() == Person.Sex.MALE
+                && p.getAge() >= 18
+                && p.getAge() <= 25,
+                p -> p.getEmailAddress(),
                 email -> System.out.println(email)
-        
         );
-                
-                
-                
-                
+
+        //to samo co powyzej ale z zastosowaniem filtrow:
+        System.out.println("Inny sposob");        
         
-
+        List<String> peopleEligibleForSelectiveService = people.stream()
+                .filter(p -> p.getGender() == Person.Sex.MALE
+                        && p.getAge() >= 18
+                        && p.getAge() <= 25)
+                .map(p-> p.getEmailAddress())
+                .collect(Collectors.toList());
+        System.out.println(peopleEligibleForSelectiveService);
     }
-
+    
 }
